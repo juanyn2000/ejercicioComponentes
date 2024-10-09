1fac636cfba945e97df73efb8988f8ebaa879c36
Cristian Ahumada Vieytes <95114277+cristiandpto23@users.noreply.github.com>
2024-10-09 20:44:54 -0300

    borrado HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ import CheckboxCr from './components/CheckboxCr.vue';
 import ContadorCr from './components/ContadorCr.vue';
 import Component6 from './components/Component6.vue';
 import Component7 from './components/Component7.vue';
-import HelloWorld from './components/HelloWorld.vue'
 
 </script>
 
